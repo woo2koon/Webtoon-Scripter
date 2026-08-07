d3ff48d93b0fdd1c812f0a30e58ede8856eaa1cc
woo2koon <wlsdn0505@gmail.com>
2026-08-07 12:25:22 +0900

    fix: Qt Rich Text 파서가 테이블(table) 태그 내부로 부모의 font-family 상속을 무시하는 버그를 테이블 인라인 스타일에 폰트를 강제 주입하여 해결

--- widgets/dialogs.py
+++ widgets/dialogs.py
@@ -1044,7 +1044,7 @@ class PreferencesDialog(QDialog):
             app_ff = QApplication.font().family() or "Pretendard"
             tooltip_html = [
                 f"<div style='margin: 4px; font-family: \"{app_ff}\";'>",
-                "  <table style='border-collapse: collapse; margin-top: 0px; min-width: 340px; font-size: 15px; border: none; line-height: 1.4;'>",
+                f"  <table style='border-collapse: collapse; margin-top: 0px; min-width: 340px; font-family: \"{app_ff}\"; font-size: 15px; border: none; line-height: 1.4;'>",
                 "    <tr style='background-color: #F8FAFC; border-bottom: 1.5px solid #CBD5E1;'>",
                 "      <th style='padding: 8px 14px; text-align: left; color: #475569; border: none; font-weight: normal;'>모델 / 엔진</th>",
                 "      <th style='padding: 8px 14px; text-align: center; color: #475569; border: none; font-weight: normal;'>사용 횟수</th>",
